e6989b8bc3e5387522b93233d23d84f1a7fedb09
3n93r <hemanth042100@gmail.com>
2025-10-22 10:32:08 +0530

    removed else block made the script bit clean

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+#taking input
 read -p "please enter the script name (at least the part of it that you remember): " wrd
 
+#counting the number of times the word was repeated
 count=$(grep -c "$wrd" /usr/share/nmap/scripts/script.db)
-
 if (( $count > 1 ));then
 echo "the word '$wrd' can is found in $count lines"
-#else
-#echo "the count val is one or less than that: $count"
 fi
 
+#printing out the lines that has the word for reference 
 if grep -q "$wrd" /usr/share/nmap/scripts/script.db;then
 grep --color=always "$wrd" /usr/share/nmap/scripts/script.db
 else
