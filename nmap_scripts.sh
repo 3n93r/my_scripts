6218681417f906bbc64225fac10cdf632d9ebf6b
3n93r <hemanth042100@gmail.com>
2025-10-22 15:54:23 +0530

    Now the I can search for more services at the same time

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-#taking input
-read -p "please enter the script name (at least the part of it that you remember): " wrd
+
+#incase you are checking what this script does then read the below line
+#the script takes in line of sentence and break all the words spearated by space into array or words
+
+read -p "enter a srting of sentences: " line
+wl=($line)
+
+#echo -e "\nthe first wrd was: ${wl[0]}"
+#echo "the number of words in that line is: ${#wl[@]}"
+
+len=${#wl[@]}
+for ((i=0;i<len;i++));do           #the loop starts here
+echo -e "For '\e[1;31m${wl[$i]}\e[0m'"
+
+wrd=${wl[$i]}
 
 #counting the number of times the word was repeated
 count=$(grep -c "$wrd" /usr/share/nmap/scripts/script.db)
@@ -15,3 +28,6 @@ grep --color=always "$wrd" /usr/share/nmap/scripts/script.db
 else
 echo "we did not find $wrd"
 fi
+
+echo ""
+done #the looop ends here
