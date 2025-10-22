45215eaf9f08c070d5d01cf5f3110b7515d8a0c1
3n93r <hemanth042100@gmail.com>
2025-10-22 15:56:24 +0530

    Cleaned up teh code a bit will have to work on next step

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-
-#incase you are checking what this script does then read the below line
-#the script takes in line of sentence and break all the words spearated by space into array or words
-
 read -p "enter a srting of sentences: " line
 wl=($line)
 
-#echo -e "\nthe first wrd was: ${wl[0]}"
-#echo "the number of words in that line is: ${#wl[@]}"
-
 len=${#wl[@]}
 for ((i=0;i<len;i++));do           #the loop starts here
+
 echo -e "For '\e[1;31m${wl[$i]}\e[0m'"
 
 wrd=${wl[$i]}
