7b4671950065d01720816ce59adcdebdfd606343
3n93r <hemanth042100@gmail.com>
2025-10-22 10:21:37 +0530

    removed extra and made changes in the count if block

--- nmap_scripts.sh
+++ nmap_scripts.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-echo -e "\n\n\n"
-#echo "hello there noting to look here, look forward to future script outputs"
-
-#read -p "please enter the script name (at least the part of it that you remember): " wrd
-
-wrd="anon"
+read -p "please enter the script name (at least the part of it that you remember): " wrd
 
 count=$(grep -c "$wrd" /usr/share/nmap/scripts/script.db)
 
 if (( $count > 1 ));then
-echo "the count val is more than one: $count"
-else
-echo "the count val is one or less than that: $count"
+echo "the word '$wrd' can is found in $count lines"
+#else
+#echo "the count val is one or less than that: $count"
 fi
 
-
 if grep -q "$wrd" /usr/share/nmap/scripts/script.db;then
-echo "okay we found $wrd in script"
 grep --color=always "$wrd" /usr/share/nmap/scripts/script.db
-
 else
 echo "we did not find $wrd"
 fi
-
-
-echo -e "\n\n\n"
